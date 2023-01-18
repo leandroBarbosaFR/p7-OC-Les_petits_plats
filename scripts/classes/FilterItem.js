9e7eca446bcb2c80dd40b48a3820e79ea3082a30
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leandrobarbosa@sfr.fr>
2023-01-18 17:20:27 +0100

    array methods

--- scripts/classes/FilterItem.js
+++
@@ -1,9 +0,0 @@
-export default class FilterItem{
-    constructor(ingredientName){
-        this.ingredientName = ingredientName;
-        this.create();
-    }
-    create(){
-        this.filterItemBlock = `<div class="filterItem">${this.ingredientName}</div>`;
-    }
-}
